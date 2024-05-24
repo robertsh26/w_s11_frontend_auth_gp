de4fdd4e31c42ba6e2c973e00ebe314fc7ff6189
Robert Shepherd <robertsh26@outlook.com>
2024-05-24 12:33:02 -0700

    first time

--- frontend/components/AuthForm.js
+++ frontend/components/AuthForm.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 export default function AuthForm() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value)
@@ -11,13 +14,14 @@ export default function AuthForm() {
     setPassword(event.target.value)
   }
   const handleSubmit = async (event) => {
+    event.preventDefault()
 
   }
 
   return (
     <div className="container">
       <div aria-live="polite"></div>
-      <div aria-live="assertive" style={{ color: 'red' }}></div>
+      <div aria-live="assertive" style={{ color: 'red' }}>{error}</div>
       <h3>Login Form</h3>
       <form onSubmit={handleSubmit}>
         <div>
